db519e994478aa20c0ba67aa9ad0a020731d1836
ivan paulino em vocacao <158524537+ivan-em-mobile@users.noreply.github.com>
2025-05-09 10:53:13 -0300

    Add files via upload

--- assets/js/app_calc.js
+++ assets/js/app_calc.js
@@ -1,28 +1,31 @@
-// Receita de 'Bolo - java script'
 
-        function insert(num) {
-            document.querySelector(".tela").innerHTML += num;
-        }
+function insert(num) {
+    document.querySelector(".tela").innerHTML += num;
 
-        function clean() {
-            document.querySelector(".tela").innerHTML = "";
-        }
+}
 
-        function back() {
-            let tela = document.querySelector(".tela").innerHTML;
-            document.querySelector(".tela").innerHTML = tela.substring(0, tela.length - 1);
-        }
+function clean() {
+    document.querySelector(".tela").innerHTML = "";
+}
 
-        function calcular() {
-            let tela = document.querySelector(".tela").innerHTML;
-            if (tela) {
-                try {
-                    document.querySelector(".tela").innerHTML = eval(tela);
-                } catch (error) {
-                    document.querySelector(".tela").innerHTML = "Erro!";
-                    console.error("Erro ao calcular:", error);
-                }
-            } else {
-                document.querySelector(".tela").innerHTML = "Sem Calculo...";
-            }
-        }
+function black() {
+    let tela = document.querySelector(".tela").innerHTML;
+    document.querySelector(".tela").innerHTML = tela.substring(0, tela.length - 1);
+}
+
+
+function calcular() {
+    let tela = document.querySelector(".tela").innerHTML;
+    
+    if (tela) {
+    
+        document.querySelector(".tela").innerHTML = eval(tela);
+    
+    } else {
+
+        document.querySelector(".tela").innerHTML = "Sem Calculo...";
+
+    }
+
+}
+
